8dc3a2588aa22ba26cd7eed5d7510cf7cd2fa5ca
The-Mad-Ryanosaurus <ryanharte1997@gmail.com>
2022-03-31 20:35:39 +0100

    Home edits

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
+import { NavController, Platform } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { InAppBrowser, InAppBrowserObject } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
+
+
+
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public platform: Platform, public navCtrl: NavController) { }
+
+  openUrlGMIT() {
+
+    this.platform.ready().then(()=>{
+      let browser = new InAppBrowserObject("https://studenthub.gmit.ie/")
+    });
+  }
 
 }
